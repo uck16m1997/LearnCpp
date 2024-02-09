115fe5fa2f5ee3ea490a6bf8308dcc565633591e
Umut Cem Kayaaltı <uckaya6@gmail.com>
2024-02-09 17:59:12 +0300

    Functions & files Update

--- CppBasics/CppBasics.cpp
+++ CppBasics/CppBasics.cpp
@@ -10,9 +10,16 @@ void myFunctionName() {}; // conventional (intercapped/CamelCase)
 
 int main()
 {
+	// std::cout is buffered and periodically, the buffer is flushed meaning all of the data collected in the buffer is transferred to its destination
+	// std::endl moves the cursor to the next line of the console, and it flushes the buffer
+	// \n is preffered since it’s more efficient to let the system flush itself periodically as it was designed
 	// Strings seperated by whitespace (space, tab, newline) will be concatenated
 	std::cout << "Hello "
 		"CppBasics!\n";
+
+	// Expressions are a sequence of literals, variables, operators, and function calls that calculates a single value
+	// Statements are a type of instruction that causes the program to perform some action
+	// An expression statement is a statement that consists of an expression followed by a semicolon examples below
 
 	int width{ 5 };    // direct list initialization of value 5 into variable width
 	int height = { 6 }; // copy list initialization of value 6 into variable height
---
+++ FunctionsAndFiles/FunctionsAndFiles.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+// Scope is a compileTime property and trying to use an identifier when it is not in scope will result in a compile error.
+int scopeFunction(int x);
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    // Return value from scopeFunction is contained in a temporary object
+    // In modern C++ (especially since C++17), the compiler has many tricks to avoid generating temporaries
+    // Since the return value of scopeFunction() is immediately output the compiler can use the return value to directly initialize the parameter of operator<<
+    std::cout << scopeFunction() << "\n";
+
+}
+
+// LifeTime is a runTime property where objects will be instantiated and destroyed
+// LifeTimes guarantee that objects must be created and initialized no later than the point of definition, and destroyed no earlier than the end 
+// Altough for optimization purposes compiler can created earlier, or destroyed later 
+// Scope is a compileTime property and trying to use an identifier when it is not in scope will result in a compile error.
+int scopeFunction() {
+    return 5;
+}
+
+// When a function is called, all of the parameters of the function are created as variables
+// Value of each of the arguments is copied into the matching parameter (using COPY INITIALIZATION)
+// This process is called PASS BY VALUE and parameters that can utilize this are called VALUE PARAMETERS
+void printValues(int x, int y)
+{
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+}
+
+void doSomething(int) // ok: unnamed parameter will not generate warning
+{
+}
